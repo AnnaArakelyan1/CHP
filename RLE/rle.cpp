8c6aaab1e38b765906237748825ac9ec2b357b44
AnnaArakelyan1 <annaarakelyan.tt219-2@polytechnic.am>
2024-10-29 20:55:02 +0400

    rle.cpp

--- RLE/rle.cpp
+++ RLE/rle.cpp
@@ -3,60 +3,83 @@
 #include <vector>
 using namespace std;
 
-
 class RLE {
-    
 public:
- 
-string encode(const string& input) {
+    string encode(const string& input) {
         string encoded;
         int count = 1;
 
         for (int i = 1; i <= input.length(); i++) {
             if (i < input.length() && input[i] == input[i - 1]) {
                 count++;
-            }
-            else {
-                encoded += input[i - 1] + to_string(count);
-                count = 1;
+            } else {
+                encoded += input[i - 1];
+
+             
+                char asciiCount[30];
+                int len = 0;
+
+                while (count > 0) {
+                    asciiCount[len++] = (count % 10) + '0';
+                    count /= 10;
+                }
+
+          
+                for (int k = 0; k < len / 2; k++) {
+                    swap(asciiCount[k], asciiCount[len - 1 - k]);
+                }
+
+           
+                asciiCount[len] = '\0';
+                encoded += asciiCount; 
+                count = 1; 
             }
         }
 
         return encoded;
     }
+    
+    
+    
+    string decode(const string& encoded) {
+    string decoded;
+
+    for (size_t i = 0; i < encoded.length();) { 
+        char currentChar = encoded[i]; 
+        i++; 
+
+        string countStr;
 
 
-string decode(const string& encoded) {
-        string decoded;
-        for (int i = 0; i < encoded.length(); i++) {
-            char currentChar = encoded[i];
-            i++;
-            string countStr;
-            while (i < encoded.length() && isdigit(encoded[i])) {
-                countStr += encoded[i];
-                i++;
-            }
-            i--; 
-
-            int count =stoi(countStr);
-            decoded.append(count, currentChar);
+        while (i < encoded.length() && isdigit(encoded[i])) {
+            countStr += encoded[i];
+            i++; 
         }
 
-        return decoded;
+        int count = 0;
+        for (char digit : countStr) {
+            count = count * 10 + (digit - '0'); 
+        }
+
+        decoded.append(count, currentChar);  
     }
-    
+
+    return decoded;
+}
+
+
     
 };
 
 int main() {
     RLE rle;
-    string input = "aaabbbccd";
+    string input = "abbbccd";
     string encoded = rle.encode(input);
     string decoded = rle.decode(encoded);
 
     cout << "Input: " << input << endl;
-    cout << "Encoded: " << encoded <<endl;
-    cout << "Decoded: " << decoded <<endl;
+    cout << "Encoded: " << encoded << endl;
+    cout << "Decoded: " << decoded << endl;
 
     return 0;
 }
