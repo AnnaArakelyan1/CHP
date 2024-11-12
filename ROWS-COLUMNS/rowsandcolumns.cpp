27d77354b7a29183251fc247d4fd3566ddb0b94b
AnnaArakelyan1 <annaarakelyan.tt219-2@polytechnic.am>
2024-11-12 20:05:53 +0400

    Update rowsandcolumns.cpp

--- ROWS-COLUMNS/rowsandcolumns.cpp
+++ ROWS-COLUMNS/rowsandcolumns.cpp
@@ -9,82 +9,90 @@ private:
     int columns;
 
 public:
-    Encryption(int Rows, int Columns) {
-        this->rows = Rows;
-        this->columns = Columns;
-    }
+    Encryption(int Rows, int Columns);
 
-    void print(const vector<vector<char>>& Str) {
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < columns; ++j) {
-                cout << Str[i][j] << " ";
-            }
-            cout << endl;
-        }
-    }
-
-    vector<vector<char>> encr(const vector<vector<char>>& Str) {
-        vector<vector<char>> newStr(columns, vector<char>(rows, ' '));
-        for (int j = 0; j < columns; ++j) {
-            for (int i = 0; i < rows; ++i) {
-                newStr[j][i] = Str[i][j];
-            }
-        }
-
-        cout << "Encrypted string:" << endl;
-        for (int j = 0; j < columns; ++j) {
-            for (int i = 0; i < rows; ++i) {
-                cout << newStr[j][i];
-            }
-        }
-        cout << endl;
-        return newStr;
-    }
-
-    vector<vector<char>> decr(const vector<vector<char>>& Str) {
-        vector<vector<char>> original(rows, vector<char>(columns, ' '));
-        for (int j = 0; j < columns; ++j) {
-            for (int i = 0; i < rows; ++i) {
-                original[i][j] = Str[j][i];
-            }
-        }
-
-        cout << "Decrypted string:" << endl;
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < columns; ++j) {
-                cout << original[i][j];
-            }
-        }
-        cout << endl;
-        return original;
-    }
+    void print(const vector<vector<char>>& Str);
+    vector<vector<char>> encr(const vector<vector<char>>& Str);
+    vector<vector<char>> decr(const vector<vector<char>>& Str);
 };
+Encryption::Encryption(int Rows, int Columns) {
+    this->rows = Rows;
+    this->columns = Columns;
+}
 
-// int main() {
-//     int SIZE;
-//     string input;
-//     cout << "Enter size: ";
-//     cin >> SIZE;
-//     cin.ignore();
-//     cout << "Enter input string: ";
-//     getline(cin, input);
-    
-//     int rows = (input.length() + SIZE - 1) / SIZE;
-//     vector<vector<char>> str(rows, vector<char>(SIZE, ' '));
+void Encryption::print(const vector<vector<char>>& Str) {
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < columns; ++j) {
+            cout << Str[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
 
-//     for (int i = 0; i < rows; i++) {
-//         for (int j = 0; j < SIZE; j++) {
-//             int index = i * SIZE + j;
-//             if (index < input.length()) {
-//                 str[i][j] = input[index];
-//             }
-//         }
-//     }
+vector<vector<char>> Encryption::encr(const vector<vector<char>>& Str) {
+    vector<vector<char>> newStr(columns, vector<char>(rows, ' '));
+    for (int j = 0; j < columns; ++j) {
+        for (int i = 0; i < rows; ++i) {
+            newStr[j][i] = Str[i][j];
+        }
+    }
 
-//     Encryption encrypt(rows, SIZE);
-//     encrypt.print(str);
-//     vector<vector<char>> encrypted = encrypt.encr(str);
-//     encrypt.decr(encrypted);
+    cout << "Encrypted string:" << endl;
+    for (int j = 0; j < columns; ++j) {
+        for (int i = 0; i < rows; ++i) {
+            cout << newStr[j][i];
+        }
+    }
+    cout << endl;
+    return newStr;
+}
 
-//     return 0;
-// }
+vector<vector<char>> Encryption::decr(const vector<vector<char>>& Str) {
+    vector<vector<char>> original(rows, vector<char>(columns, ' '));
+    for (int j = 0; j < columns; ++j) {
+        for (int i = 0; i < rows; ++i) {
+            original[i][j] = Str[j][i];
+        }
+    }
+
+    cout << "Decrypted string:" << endl;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < columns; ++j) {
+            cout << original[i][j];
+        }
+    }
+    cout << endl;
+    return original;
+}
+
+
+//int main() {
+//    int SIZE;
+//    string input;
+//    cout << "Enter size: ";
+//    cin >> SIZE;
+//    cin.ignore();
+//    cout << "Enter input string: ";
+//    getline(cin, input);
+//
+//    
+//    int rows = (input.length() + SIZE - 1) / SIZE;
+//    vector<vector<char>> str(rows, vector<char>(SIZE, ' '));
+//
+//   
+//    for (int i = 0; i < rows; i++) {
+//        for (int j = 0; j < SIZE; j++) {
+//            int index = i * SIZE + j;
+//            if (index < input.length()) {
+//                str[i][j] = input[index];
+//            }
+//        }
+//    }
+//
+//    Encryption encrypt(rows, SIZE);
+//    encrypt.print(str); 
+//    vector<vector<char>> encrypted = encrypt.encr(str);
+//    encrypt.decr(encrypted);  
+//
+//    return 0;
+//}
