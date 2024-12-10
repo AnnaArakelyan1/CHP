95db90af661eed2248546c6b8e4e4f0a36ce101b
AnnaArakelyan1 <annaarakelyan.tt219-2@polytechnic.am>
2024-12-10 23:01:51 +0400

    Update mainClient.cpp

--- lab11/mainClient.cpp
+++ lab11/mainClient.cpp
@@ -3,78 +3,60 @@
 #include "UDPSocket.h"
 #include <cstring>  
 #define _CRT_SECURE_NO_WARNINGS
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+#define PORT 8888
+#define BUFLEN 512
 
-int main(int argc, char* argv[])
-{
-    struct sockaddr_in si_other;
-    int slen = sizeof(si_other);
-    char buf[BUFLEN];
+int main() {
+    WSADATA wsa;
+    SOCKET clientSock;
+    struct sockaddr_in serverAddr;
     char message[BUFLEN];
+    char buffer[BUFLEN];
+    int slen = sizeof(serverAddr);
 
-    unsigned short srv_port = 8888;
-    char srv_ip_addr[40] = "127.0.0.1"; 
-
-    if (argc == 3) {
-     
-        strncpy_s(srv_ip_addr, argv[1], sizeof(srv_ip_addr) - 1);
-        srv_ip_addr[sizeof(srv_ip_addr) - 1] = '\0'; 
-        srv_port = static_cast<unsigned short>(std::stoi(argv[2]));
-    }
-    else {
-        std::cout << "Usage: " << argv[0] << " <Server IP> <Server Port>" << std::endl;
-        return -1;
+   
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+        std::cerr << "WSAStartup failed with error: " << WSAGetLastError() << std::endl;
+        return 1;
     }
 
-    UDPSocket client_sock;
-    memset((char*)&si_other, 0, sizeof(si_other));
-    si_other.sin_family = AF_INET;
-    si_other.sin_port = htons(srv_port);
-
-    if (inet_pton(AF_INET, srv_ip_addr, &si_other.sin_addr) <= 0) {
-        std::cout << "Invalid address/ Address not supported: " << srv_ip_addr << std::endl;
-        return -1;
+ 
+    clientSock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (clientSock == INVALID_SOCKET) {
+        std::cerr << "Socket creation failed with error: " << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return 1;
     }
 
-    std::cout << "Client will connect to server at " << srv_ip_addr << ":" << srv_port << std::endl;
 
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
     while (true) {
-        std::cout << "\nEnter message: ";
+        std::cout << "Enter message: ";
+        std::cin.getline(message, BUFLEN);
 
-     
-        if (!std::cin.good()) {
-            std::cerr << "Input stream error. Exiting." << std::endl;
-            break;
-        }
-
-        std::string input;
-        std::getline(std::cin, input);
-
-        if (input.empty()) {
-            std::cout << "Empty message entered. Please try again." << std::endl;
+ 
+        if (sendto(clientSock, message, strlen(message), 0, (struct sockaddr*)&serverAddr, slen) == SOCKET_ERROR) {
+            std::cerr << "sendto failed with error: " << WSAGetLastError() << std::endl;
             continue;
         }
 
-      
-        strncpy_s(message, input.c_str(), BUFLEN - 1);
-        message[BUFLEN - 1] = '\0';
-
-        std::cout << "Sending message: " << message << std::endl;
-
-
-        client_sock.SendDatagram(message, strlen(message), (struct sockaddr*)&si_other, slen);
-
-   
-        memset(buf, '\0', BUFLEN);
-        int receivedBytes = client_sock.RecvDatagram(buf, BUFLEN, (struct sockaddr*)&si_other, &slen);
-
-        if (receivedBytes > 0) {
-            buf[receivedBytes] = '\0';  
-            std::cout << "Received from server: " << buf << std::endl;
+     
+        int receivedBytes = recvfrom(clientSock, buffer, BUFLEN, 0, (struct sockaddr*)&serverAddr, &slen);
+        if (receivedBytes == SOCKET_ERROR) {
+            std::cerr << "recvfrom failed with error: " << WSAGetLastError() << std::endl;
         }
         else {
-            std::cerr << "No response received or an error occurred." << std::endl;
+            buffer[receivedBytes] = '\0';
+            std::cout << "Received from server: " << buffer << std::endl;
         }
     }
 
+    closesocket(clientSock);
+    WSACleanup();
     return 0;
 }
