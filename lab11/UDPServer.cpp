9ee413cec8dd449b9aa76164d01fb59065536b84
AnnaArakelyan1 <annaarakelyan.tt219-2@polytechnic.am>
2024-12-15 20:59:03 +0400

    Update UDPServer.cpp

--- lab11/UDPServer.cpp
+++ lab11/UDPServer.cpp
@@ -1,46 +1,34 @@
 #include "UDPServer.h"
-#include <stdexcept>
+#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
 
-UDPServer::UDPServer(unsigned short port) {
-
-    WSADATA wsa;
-    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
-        throw std::runtime_error("WSAStartup failed");
-    }
+UDPServer::UDPServer(SOCKET socket) : serverSock(socket) {}
 
 
-    if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == SOCKET_ERROR) {
-        throw std::runtime_error("Socket creation failed");
-    }
-
-
-    si_server.sin_family = AF_INET;
-    si_server.sin_port = htons(port);
-    si_server.sin_addr.s_addr = INADDR_ANY;
-
-
-    if (bind(s, (struct sockaddr*)&si_server, sizeof(si_server)) == SOCKET_ERROR) {
-        throw std::runtime_error("Bind failed");
+UDPServer::UDPServer() {
+    serverSock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (serverSock == INVALID_SOCKET) {
+        std::cerr << "Socket creation failed." << std::endl;
     }
 }
+
 
 UDPServer::~UDPServer() {
-    closesocket(s);
-    WSACleanup();
+    closesocket(serverSock);
 }
 
-int UDPServer::RecvDatagram(char* buf, unsigned int buflen, struct sockaddr* si_dest, int* slen) {
-    int receivedBytes = recvfrom(s, buf, buflen, 0, si_dest, slen);
-    if (receivedBytes == SOCKET_ERROR) {
-        throw std::runtime_error("recvfrom failed");
-    }
-    return receivedBytes;
+
+int UDPServer::Bind(struct sockaddr* addr, int addrlen) {
+    return bind(serverSock, addr, addrlen);
 }
 
-int UDPServer::SendDatagram(const char* msg, unsigned int msglen, struct sockaddr* si_dest, unsigned int slen) {
-    int sentBytes = sendto(s, msg, msglen, 0, si_dest, slen);
-    if (sentBytes == SOCKET_ERROR) {
-        throw std::runtime_error("sendto failed");
-    }
-    return sentBytes;
+
+int UDPServer::SendDatagram(const char* buf, int len, struct sockaddr* addr, int addrlen) {
+    return sendto(serverSock, buf, len, 0, addr, addrlen);
+}
+
+
+int UDPServer::RecvDatagram(char* buf, int len, struct sockaddr* addr, int* addrlen) {
+    return recvfrom(serverSock, buf, len, 0, addr, addrlen);
 }
