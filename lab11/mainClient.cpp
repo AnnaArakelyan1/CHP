2a5bdacc24a9f064789f7227c568d46a65b249bb
AnnaArakelyan1 <annaarakelyan.tt219-2@polytechnic.am>
2024-12-15 20:57:01 +0400

    Update mainClient.cpp

--- lab11/mainClient.cpp
+++ lab11/mainClient.cpp
@@ -1,62 +1,97 @@
-#include <iostream>
-#include <sstream>  
 #include "UDPSocket.h"
-#include <cstring>  
-#define _CRT_SECURE_NO_WARNINGS
-#include <winsock2.h>
 #include <ws2tcpip.h>
-#pragma comment(lib, "ws2_32.lib")
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+using namespace std;
+
+
+#define SERVER "127.0.0.1"
 #define PORT 8888
 #define BUFLEN 512
 
-int main() {
-    WSADATA wsa;
-    SOCKET clientSock;
-    struct sockaddr_in serverAddr;
-    char message[BUFLEN];
-    char buffer[BUFLEN];
-    int slen = sizeof(serverAddr);
 
-   
-    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
-        std::cerr << "WSAStartup failed with error: " << WSAGetLastError() << std::endl;
-        return 1;
+#define THREAD_LOCK std::mutex
+#define THREADFUNCVAR void*
+#define THREADFUNCARGS void*
+#define THREADVAR std::thread
+
+
+THREAD_LOCK recv_lock;
+bool quitnow = false;
+
+
+void LockThread(THREAD_LOCK& lock) {
+    lock.lock();
+}
+
+void UnlockThread(THREAD_LOCK& lock) {
+    lock.unlock(); 
+}
+
+void InitThreadLock(THREAD_LOCK& lock) {
+
+}
+
+
+void ReceiveMessagesThread(UDPSocket* client_sock, sockaddr_in* si_other) {
+    int slen = sizeof(sockaddr_in);
+    char buf[BUFLEN] = { 0 };
+
+    while (!quitnow) {
+        memset(buf, 0, BUFLEN); 
+
+        LockThread(recv_lock); 
+        int bytesReceived = client_sock->RecvDatagram(buf, BUFLEN, (struct sockaddr*)si_other, &slen);
+        UnlockThread(recv_lock); 
+
+        if (bytesReceived > 0) {
+            cout << "\n[Server]: " << buf << endl;
+        }
+        else if (bytesReceived == SOCKET_ERROR) {
+            cerr << "Error receiving message: " << WSAGetLastError() << endl;
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));  
+    }
+}
+
+
+int main(int argc, char* argv[]) {
+    struct sockaddr_in si_other;
+    int slen = sizeof(si_other);
+    char message[BUFLEN] = { 0 };
+
+    UDPSocket client_sock;
+    InitThreadLock(recv_lock); 
+
+
+    memset((char*)&si_other, 0, sizeof(si_other));
+    si_other.sin_family = AF_INET;
+    si_other.sin_port = htons((argc > 1) ? atoi(argv[1]) : PORT);
+    inet_pton(AF_INET, (argc > 2) ? argv[2] : SERVER, &si_other.sin_addr);
+
+    cout << "Client started. Connected to server " << SERVER << " on port " << PORT << endl;
+
+
+    THREADVAR recvThread(ReceiveMessagesThread, &client_sock, &si_other);
+
+
+    while (!quitnow) {
+        cout << "Enter message: ";
+        cin.getline(message, BUFLEN);
+
+        LockThread(recv_lock); 
+        client_sock.SendDatagram(message, (int)strlen(message), (struct sockaddr*)&si_other, slen);
+        UnlockThread(recv_lock);  
+
+        if (string(message) == "exit") {
+            quitnow = true;
+        }
     }
 
- 
-    clientSock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if (clientSock == INVALID_SOCKET) {
-        std::cerr << "Socket creation failed with error: " << WSAGetLastError() << std::endl;
-        WSACleanup();
-        return 1;
-    }
+  
+    recvThread.join();
 
-
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(PORT);
-    inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
-    while (true) {
-        std::cout << "Enter message: ";
-        std::cin.getline(message, BUFLEN);
-
- 
-        if (sendto(clientSock, message, strlen(message), 0, (struct sockaddr*)&serverAddr, slen) == SOCKET_ERROR) {
-            std::cerr << "sendto failed with error: " << WSAGetLastError() << std::endl;
-            continue;
-        }
-
-     
-        int receivedBytes = recvfrom(clientSock, buffer, BUFLEN, 0, (struct sockaddr*)&serverAddr, &slen);
-        if (receivedBytes == SOCKET_ERROR) {
-            std::cerr << "recvfrom failed with error: " << WSAGetLastError() << std::endl;
-        }
-        else {
-            buffer[receivedBytes] = '\0';
-            std::cout << "Received from server: " << buffer << std::endl;
-        }
-    }
-
-    closesocket(clientSock);
-    WSACleanup();
     return 0;
 }
