5755b2198ce39bc2952246cdf3e0ed68535a8b31
AnnaArakelyan1 <annaarakelyan.tt219-2@polytechnic.am>
2024-12-15 20:55:14 +0400

    Update mainServer.cpp

--- lab11/mainServer.cpp
+++ lab11/mainServer.cpp
@@ -1,42 +1,69 @@
 #include <iostream>
-#include <cstring>  
+#include <cstring>
+#include <string>
 #include <thread>
-#include <ws2tcpip.h> 
-#include "UDPServer.h"
-#define PORT 8888  
+#include <ws2tcpip.h>
 #include <winsock2.h>
-#pragma comment(lib, "ws2_32.lib")
+#include <mutex>
+#include "UDPServer.h"
+
+#define PORT 8888  
 #define BUFLEN 512
+#pragma comment(lib, "ws2_32.lib")
+
+
+std::mutex recv_lock; 
+bool quitnow = false;
+
+
+void LockThread(std::mutex& lock) {
+    lock.lock();  
+}
+
+
+void UnlockThread(std::mutex& lock) {
+    lock.unlock();  
+}
+
 
 void handleClient(UDPServer& server) {
     struct sockaddr_in si_other;
     int slen = sizeof(si_other);
     char buf[BUFLEN];
     char ipStr[INET_ADDRSTRLEN];
-    const char* prompt = "Enter message: ";
 
-    while (true) {
+    while (!quitnow) {
         int receivedBytes = server.RecvDatagram(buf, BUFLEN, (struct sockaddr*)&si_other, &slen);
         if (receivedBytes > 0) {
             buf[receivedBytes] = '\0'; 
             inet_ntop(AF_INET, &si_other.sin_addr, ipStr, INET_ADDRSTRLEN);
 
+    
+            LockThread(recv_lock);
+
             std::cout << "Received packet from " << ipStr << ":" << ntohs(si_other.sin_port) << std::endl;
             std::cout << "Data: " << buf << std::endl;
 
-           
-            std::cout << "Sending response: " << prompt << std::endl;
-            int sentBytes = server.SendDatagram(prompt, strlen(prompt), (struct sockaddr*)&si_other, slen);
+            UnlockThread(recv_lock);
+
+          
+            std::string response = "Received message: " + std::string(buf);
+            std::cout << "Sending response: " << response << std::endl;
+
+            int sentBytes = server.SendDatagram(response.c_str(), response.length(), (struct sockaddr*)&si_other, slen);
             if (sentBytes == -1) {
                 std::cerr << "Failed to send response. Error code: " << WSAGetLastError() << std::endl;
             }
+
+       
+            std::string prompt = "Enter a text: ";
+            server.SendDatagram(prompt.c_str(), prompt.length(), (struct sockaddr*)&si_other, slen);
         }
         else {
             std::cerr << "Failed to receive data. Error code: " << WSAGetLastError() << std::endl;
         }
     }
 }
-
 
 int main() {
     WSADATA wsa;
@@ -59,11 +86,12 @@ int main() {
         return 1;
     }
 
+
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(PORT);
     serverAddr.sin_addr.s_addr = INADDR_ANY;
 
-
+  
     if (bind(serverSock, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
         std::cerr << "Bind failed with error: " << WSAGetLastError() << std::endl;
         closesocket(serverSock);
@@ -72,20 +100,27 @@ int main() {
     }
 
     std::cout << "Server listening on port " << PORT << "...\n";
-    while (true) {
-        int receivedBytes = recvfrom(serverSock, buffer, BUFLEN, 0, (struct sockaddr*)&clientAddr, &slen);
-        if (receivedBytes == SOCKET_ERROR) {
-            std::cerr << "recvfrom failed with error: " << WSAGetLastError() << std::endl;
-            continue;
-        }
 
-        buffer[receivedBytes] = '\0';
-        std::cout << "Received message: " << buffer << std::endl;
+  
+    UDPServer server(serverSock);
+    std::thread clientHandler(handleClient, std::ref(server));
 
+    
+    while (!quitnow) {
+        std::string userInput;
+  
+        std::getline(std::cin, userInput);  
 
-        const char* response = "Message received";
-        sendto(serverSock, response, strlen(response), 0, (struct sockaddr*)&clientAddr, slen);
+    
+        LockThread(recv_lock);
+        const char* response = userInput.c_str();
+        sendto(serverSock, response, userInput.length(), 0, (struct sockaddr*)&clientAddr, slen);
+        UnlockThread(recv_lock); 
     }
+
+
+    quitnow = true;
+    clientHandler.join();  
 
     closesocket(serverSock);
     WSACleanup();
